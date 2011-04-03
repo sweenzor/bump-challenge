caaf9bc02b11e18103125feaa6a390d696a2ffce
Matt Sweeney <mattswe@gmail.com>
2011-04-03 13:50:43 -0700

    bump json parse into EMR stream format

--- bump-json.py
+++ bump-json.py
@@ -1,9 +1,21 @@
+#!/usr/bin/python
+
+import sys
 import json
 
-logs = open('bump10.log')
-lines = logs.readlines()
-for line in lines:
-    if line.split(' ')[2]=="MATCH":
+#logs = open('bump10.log')
+#lines = logs.readlines()
+
+def main(argv):
+  line = sys.stdin.readline()
+  try:
+    while line:
+      if line.split(' ')[2]=="MATCH":
         dict1 = json.loads(line[28:])
-        print dict1['userid1']
-        print dict1['userid2']
+        print "LongValueSum:" + dict1['userid1'] + "\t" + "1"
+        print "LongValueSum:" + dict1['userid2'] + "\t" + "1"
+      line = sys.stdin.readline()
+  except "end of file":
+    return None
+if __name__ == "__main__":
+  main(sys.argv)
