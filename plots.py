d814836dd9a60b249f552278ed8a2de159eecf95
Matt Sweeney <mattswe@gmail.com>
2011-04-05 16:03:53 -0700

    plotting hist results as curve

--- plots.py
+++ plots.py
@@ -9,24 +9,11 @@ count = []
 for line in lines:
     count.append(int(line.split('\t')[1]))
 
-x = pylab.array(count)
-n, bins, patches = pylab.hist(x, 200, log=True)
-pylab.plot(n)
-
-#pylab.plot(x,[idx for idx, value in enumerate(x)])
-
-#poly = numpy.poly1d(numpy.polyfit([idx for idx, value in enumerate(x)],x,5))
-#pylab.plot(x, poly(x), '-k', linewidth=2)
+x = pylab.sort(pylab.array(count))
+n, bins, patches = pylab.hist(x[1:], 200, log=True)
 
 
-
-mu = pylab.mean(x)
-sigma = pylab.std(x)
-bincenters = 0.5*(bins[1:]+bins[:-1])
-# add a 'best fit' line for the normal PDF
-y = mlab.normpdf( bincenters, mu, sigma )
-pylab.plot(bincenters, y, 'r--', linewidth=1)
-
+pylab.plot(range(1,len(n)+1),n)
 
 
 pylab.show()
