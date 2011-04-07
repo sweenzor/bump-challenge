1b124b5cc10e3e553d2afc497c7f7accab8837f3
Matt Sweeney <mattswe@gmail.com>
2011-04-07 00:41:52 -0700

    added platform classification

--- bump-reducer.py
+++ bump-reducer.py
@@ -12,24 +12,33 @@ def main(argv):
       if dict1['head'] == "MATCH":
         id1 = dict1['userid1']
         id2 = dict1['userid2']
-        count[id1] = 1 
-        count[id2] = 2 
-        #if id1 in count:
-          #try:
-            #count[id1]['matches'][id2] = count.get(id1).get('matches').get(id2,0) + 1
-          #except:
-            #count[id1] = {'matches':{id2:1}}
+        
+        if id1 in count:
+          count[id1]['matches'][id2] = count.get(id1).get('matches').get(id2,0) + 1
+        else:
+          count[id1] = {'matches':{id2:1}}
       
-        #if id2 in count:
-          #try:
-            #count[id2]['matches'][id1] = count.get(id2).get('matches').get(id1,0) + 1
-          #except:
-            #count[id2] = {'matches':{id1:1}}
-
-        print count
+        if id2 in count:
+          count[id2]['matches'][id1] = count.get(id2).get('matches').get(id1,0) + 1
+        else:
+          count[id2] = {'matches':{id1:1}}
+      
+      if dict1['head'] == "HELLO":
+        id1 = dict1['userid']  
+        try:
+          count[id1]['platform'] = dict1['platform']
+        except:
+          pass       
+ 
       line = sys.stdin.readline()
   except:
     pass
+  
+  for uid in count:
+    print uid, '\t',len(count[uid]['matches'])
+  print count
+
+
 if __name__ == "__main__":
   main(sys.argv)
 
