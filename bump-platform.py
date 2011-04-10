f3fc7ac37565136158d06ea80cbd775023f32e73
Matt Sweeney <mattswe@gmail.com>
2011-04-10 04:36:44 -0700

    adding bump-platform

---
+++ bump-platform.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import sys
+import simplejson as json
+
+def extract(d, keys):
+  return dict((k, d[k]) for k in keys if k in d)
+
+def main(argv):
+
+  line = sys.stdin.readline()
+  try:
+    while line:
+    
+      #if line.split(' ')[2]=="MATCH":
+        #dict1 = json.loads(line[28:])
+        #dict2 = {}
+        #dict2.update(extract(dict1,['userid1','userid2']))
+        #dict2['head'] = 'MATCH'
+
+        #sys.stdout.write(json.dumps(dict2, sort_keys=True)+'\n')
+        
+
+      if line.split(' ')[2]=="HELLO":
+        dict1 = json.loads(line[28:])
+        dict2 = {}
+        dict2.update(extract(dict1,['userid']))
+        dict2.update(extract(dict1['app_version'],['platform']))
+        dict2['head'] = 'HELLO'
+
+        sys.stdout.write(json.dumps(dict2, sort_keys=True)+'\n')
+
+      line = sys.stdin.readline()
+
+  except:
+    pass
+
+if __name__ == "__main__":
+  main(sys.argv)
