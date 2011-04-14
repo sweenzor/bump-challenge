75701062fe5f06a0f5dc4a9997c5a2950ee36e3f
Matt Sweeney <mattswe@gmail.com>
2011-04-13 21:09:14 -0700

    updated hist

--- plots.py
+++ plots.py
@@ -4,7 +4,7 @@ from matplotlib import mlab
 from matplotlib import pyplot
 
 
-file = open('part-00001.log')
+file = open('mapreduced-data/matches.log')
 lines = file.readlines()
 
 count = []
@@ -12,13 +12,14 @@ for line in lines:
     count.append(int(line.split('\t')[1]))
 
 x = pylab.sort(pylab.array(count))
-n, bins, patches = pylab.hist(x, 200, log=True)
+#n, bins, patches = pylab.hist(x, 200, log=True)
 n, bins =  numpy.histogram(x,max(count))
 
 fig = pyplot.figure()
 ax = fig.add_subplot(111)
 
 ax.semilogy(range(1,len(n)+1),n)
+ax.set_xlim([0,30])
 pyplot.show()
 
 
