bf3540958fa6fdf93986eb27f3c0a876c204c526
Matt Sweeney <mattswe@gmail.com>
2011-04-13 21:45:49 -0700

    using lookup table to sort entries

--- plots.py
+++ plots.py
@@ -6,10 +6,17 @@ from matplotlib import pyplot
 
 file = open('mapreduced-data/matches.log')
 lines = file.readlines()
+file.close()
 
 count = []
 for line in lines:
     count.append(int(line.split('\t')[1]))
+
+id_with_count = []
+for line in lines:
+    ids = line.strip().split('\t')
+    id_with_count.append([int(ids[0]), int(ids[1])])
+
 
 x = pylab.sort(pylab.array(count))
 #n, bins, patches = pylab.hist(x, 200, log=True)
@@ -23,4 +30,37 @@ ax.set_xlim([0,30])
 pyplot.show()
 
 
+
+file = open('mapreduced-data/platform.log')
+plat = file.readlines()
+file.close
+
+platform = {}
+for line in plat:
+    entry = line.strip().split('\t')
+    platform[int(entry[0])]=entry[1]
+
+
+count_android = []
+count_ios = []
+false = 0
+
+for entry in id_with_count:
+    try:
+        if platform[entry[0]] == 'iOS':
+            count_ios.append(entry[1])
+    except:
+        false += 1
+
+    try:
+        if platform[entry[0]] == 'Android':
+            count_android.append(entry[1])
+    except:
+        false += 1
+
+
+print len(count_android)
+print len(count_ios)
+print len(count)
+print false
 
